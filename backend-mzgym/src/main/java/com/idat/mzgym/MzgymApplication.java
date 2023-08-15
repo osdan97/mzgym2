931f04cb44e2a42ff07d5b1afefd56f769fb01ac
osdan97 <daniejurade@gmail.com>
2023-08-15 18:08:16 -0500

    main datasource path

--- backend-mzgym/src/main/java/com/idat/mzgym/MzgymApplication.java
+++ backend-mzgym/src/main/java/com/idat/mzgym/MzgymApplication.java
@@ -2,10 +2,11 @@ package com.idat.mzgym;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class MzgymApplication {
 
 
