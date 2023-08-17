88e7074dfcb7a4a31183356f01c16a3fda676ba7
ander <86135000+anderssongodoy@users.noreply.github.com>
2023-08-17 02:29:55 -0500

    Update MembershipController.java

--- backend-mzgym/src/main/java/com/idat/mzgym/controller/MembershipController.java
+++ backend-mzgym/src/main/java/com/idat/mzgym/controller/MembershipController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping("api/membership")
 public class MembershipController {
     @Autowired
