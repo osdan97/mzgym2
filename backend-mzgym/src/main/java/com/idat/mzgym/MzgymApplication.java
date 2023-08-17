a971f8b0e6569af0ec99901105708e3e124ea7cc
osdan97 <daniejurade@gmail.com>
2023-08-17 17:44:19 -0500

    corss origin

--- backend-mzgym/src/main/java/com/idat/mzgym/MzgymApplication.java
+++ backend-mzgym/src/main/java/com/idat/mzgym/MzgymApplication.java
@@ -9,7 +9,15 @@ import org.springframework.context.annotation.Bean;
 @SpringBootApplication
 public class MzgymApplication {
 
+    @Bean
+    public WebMvcConfigurer corsConfigurer(){
 
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("*");
+            }
+        };}
 
     public static void main(String[] args) {
 
