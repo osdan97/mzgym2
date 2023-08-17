395247f4b324807c9f4080ca57786c68a5414b79
anderssongodoy <anderssongodoygarcia@gmail.com>
2023-08-17 13:31:34 -0500

    Package Cors

--- backend-mzgym/src/main/java/com/idat/mzgym/config/CrossOriginConfig.java
+++ backend-mzgym/src/main/java/com/idat/mzgym/config/CrossOriginConfig.java
@@ -1,14 +1,15 @@
-package com.nocountry.ecommerce.config;
+package com.idat.mzgym.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 @Configuration
 public class CrossOriginConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000/")
+                .allowedOrigins("http://localhost:3000")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT")
                 .allowedHeaders("*");
 
